ed947ce2c89f80c57f555c7636e280cc9d871c4f
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2026-05-05 22:33:07 +0530

    feat: first simple Task component + Story

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,8 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.@(ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
     "@chromatic-com/storybook",
